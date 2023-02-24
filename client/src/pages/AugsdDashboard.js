1d33d56e8ce8ab47d1d835537c451b243bf188d3
JaPyC <khooshrin123@gmail.com>
2023-02-24 10:47:18 +0530

    AUGSD Scroll Issue Fixed

--- client/src/pages/AugsdDashboard.js
+++ client/src/pages/AugsdDashboard.js
@@ -19,6 +19,7 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import NewProjectTable from '../components/NewProjectTable';
 import ApprovedProjectTable from '../components/ApprovedProjectTable';
 import LogoutIcon from '@mui/icons-material/Logout';
+import { maxWidth } from '@mui/system';
 
 
 const drawerWidth = 240;
@@ -93,18 +94,7 @@ function DashboardContent() {
               pr: '24px', // keep right padding when drawer closed
             }}
           >
-            {/* <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: '36px',
-                ...(open && { display: 'none' }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton> */}
+           
             <Typography
               component="h1"
               variant="h6"
@@ -115,33 +105,10 @@ function DashboardContent() {
               AUGSD Dashboard
             </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit}>
-              {/* <IconButton color="inherit">
-                <LogoutIcon />
-              </IconButton> */}
               <Button color="inherit" size="large" startIcon={<LogoutIcon />} type="submit">LogOut</Button>
             </Box>
           </Toolbar>
         </AppBar>
-        {/* <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <List component="nav">
-            {mainListItems}
-            <Divider sx={{ my: 1 }} />
-            {secondaryListItems}
-          </List>
-        </Drawer> */}
         <Box
           component="main"
           sx={{
@@ -150,12 +117,10 @@ function DashboardContent() {
                 ? theme.palette.grey[100]
                 : theme.palette.grey[900],
             flexGrow: 1,
-            height: '100vh',
             overflow: 'auto',
-            marginTop: 5,
+            marginTop: 8,
           }}
         >
-          {/* <Toolbar /> */}
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={3}>
               <Grid item xs={12}>
--- client/src/pages/AugsdLogin.js
+++ client/src/pages/AugsdLogin.js
@@ -41,28 +41,8 @@ const AugsdLogin = () => {
     }
 
     return (
-        // <form className="login" onSubmit={handleSubmit}>
-        //     <h3>Login</h3>
-        //     <label>Email</label>
-        //     <input 
-        //         type="email"
-        //         onChange={(e) => setEmail(e.target.value)}
-        //         value={email}
-        //     />
-
-        //     <label>Password</label>
-        //     <input 
-        //         type="password"
-        //         onChange={(e) => setPassword(e.target.value)}
-        //         value={password}
-        //     />
-
-        //     <button disabled={isLoading}>Login</button>
-        //     {error && <div className="error">{error}</div>}
-        // </form>
-
         <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: '90vh' }}>
         <CssBaseline />
         <Grid
           item
