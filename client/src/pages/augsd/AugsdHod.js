3ba6aa870cc37a7a7fd984321a7326db06685364
Kavyanjali Agnihotri <kavyagnihotri17@gmail.com>
2023-03-20 22:55:02 +0530

    Add home button to student login

--- client/src/pages/augsd/AugsdHod.js
+++ client/src/pages/augsd/AugsdHod.js
@@ -1,107 +1,116 @@
-import * as React from 'react';
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
-import { useLogout } from '../../hooks/useLogout'
-import CssBaseline from '@mui/material/CssBaseline';
-import Button from '@mui/material/Button';
-import MuiDrawer from '@mui/material/Drawer';
-import Box from '@mui/material/Box';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import CurrentHod from '../../components/CurrentHod';
-import RemainingProfs from '../../components/RemainingProfs';
-import LogoutIcon from '@mui/icons-material/Logout';
-import HomeIcon from '@mui/icons-material/Home';
-import HowToRegIcon from '@mui/icons-material/HowToReg';
-import { useNavigate } from 'react-router-dom'
-import { AppBar } from '../../components/dashboard/AppBar'
+import * as React from "react"
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles"
+import { useLogout } from "../../hooks/useLogout"
+import CssBaseline from "@mui/material/CssBaseline"
+import Button from "@mui/material/Button"
+import MuiDrawer from "@mui/material/Drawer"
+import Box from "@mui/material/Box"
+import MuiAppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import Container from "@mui/material/Container"
+import Grid from "@mui/material/Grid"
+import Paper from "@mui/material/Paper"
+import CurrentHod from "../../components/CurrentHod"
+import RemainingProfs from "../../components/RemainingProfs"
+import LogoutIcon from "@mui/icons-material/Logout"
+import HomeIcon from "@mui/icons-material/Home"
+import HowToRegIcon from "@mui/icons-material/HowToReg"
+import { useNavigate } from "react-router-dom"
+import { AppBar } from "../../components/dashboard/Objects"
 
-const mdTheme = createTheme();
+const mdTheme = createTheme()
 
 function HodContent() {
-  const { logout } = useLogout()
-  const navigate = useNavigate()
+    const { logout } = useLogout()
+    const navigate = useNavigate()
 
-  const handleSubmit = async(e) => {
-    e.preventDefault()
-    logout()
-  }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        logout()
+    }
 
-  const goHome = async(e) => {
-    e.preventDefault()
-    navigate("/augsd/dashboard");
-  }
+    const goHome = async (e) => {
+        e.preventDefault()
+        navigate("/augsd/dashboard")
+    }
 
-  const goHOD = async(e) => {
-    e.preventDefault()
-    navigate("/augsd/hod");
-  }
-  
-  return (
-    <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar position="absolute" >
-          <Toolbar
-            sx={{
-              justifyContent:"space-between"
-            }}
-          >
-              <Button color="inherit" size="large" startIcon={<HomeIcon />} type="submit"
-              variant="h6"
-              noWrap
-              onClick={goHome}>
-                <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-            >
-              Back to AUGSD Dashboard
-            </Typography>
-              </Button>
-              <Button color="inherit" size="large" startIcon={<HowToRegIcon />} type="submit"onClick={goHOD} >Mark HOD</Button>
-              <Button color="inherit" size="large" startIcon={<LogoutIcon />} onClick={handleSubmit} type="submit" >LogOut</Button>
-          </Toolbar>
-        </AppBar>
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            overflow: 'auto',
-            marginTop: 8,
-          }}
-        >
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                  <CurrentHod /> 
-                </Paper>
-              </Grid>
-            </Grid>
-          </Container>
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                  <RemainingProfs /> 
-                </Paper>
-              </Grid>
-            </Grid>
-          </Container>
-        </Box>
-      </Box>
-    </ThemeProvider>
-  );
+    const goHOD = async (e) => {
+        e.preventDefault()
+        navigate("/augsd/hod")
+    }
+
+    return (
+        <ThemeProvider theme={mdTheme}>
+            <Box sx={{ display: "flex" }}>
+                <CssBaseline />
+                <AppBar position="absolute">
+                    <Toolbar
+                        sx={{
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        <Button
+                            color="inherit"
+                            size="large"
+                            startIcon={<HomeIcon />}
+                            type="submit"
+                            variant="h6"
+                            noWrap
+                            onClick={goHome}
+                        >
+                            <Typography component="h1" variant="h6" color="inherit">
+                                Back to AUGSD Dashboard
+                            </Typography>
+                        </Button>
+                        <Button color="inherit" size="large" startIcon={<HowToRegIcon />} type="submit" onClick={goHOD}>
+                            Mark HOD
+                        </Button>
+                        <Button
+                            color="inherit"
+                            size="large"
+                            startIcon={<LogoutIcon />}
+                            onClick={handleSubmit}
+                            type="submit"
+                        >
+                            LogOut
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+                <Box
+                    component="main"
+                    sx={{
+                        backgroundColor: (theme) =>
+                            theme.palette.mode === "light" ? theme.palette.grey[100] : theme.palette.grey[900],
+                        flexGrow: 1,
+                        overflow: "auto",
+                        marginTop: 8,
+                    }}
+                >
+                    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                        <Grid container spacing={3}>
+                            <Grid item xs={12}>
+                                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                                    <CurrentHod />
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                        <Grid container spacing={3}>
+                            <Grid item xs={12}>
+                                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                                    <RemainingProfs />
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Box>
+            </Box>
+        </ThemeProvider>
+    )
 }
 
 export default function AugsdHod() {
-  return <HodContent />;
+    return <HodContent />
 }
--- client/src/pages/professor/ProfDashboard.js
+++ client/src/pages/professor/ProfDashboard.js
@@ -1,168 +1,139 @@
-import * as React from "react";
-import { useEffect, useState } from 'react'
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import LogoutIcon from "@mui/icons-material/Logout";
-import Button from "@mui/material/Button";
-import { TableContainer } from "@mui/material";
-import { mainListItems, secondaryListItems, } from "../../components/dashboard/profListItems";
-import Projects from "../../components/project/ProfProjects";
-import { AppBar } from "../../components/dashboard/AppBar";
-import { Drawer } from "../../components/dashboard/Drawer";
-import { useAuthContext } from "../../hooks/useAuthContext";
-import { useLogout } from "../../hooks/useLogout";
+import * as React from "react"
+import { useEffect, useState } from "react"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+import Box from "@mui/material/Box"
+import Toolbar from "@mui/material/Toolbar"
+import List from "@mui/material/List"
+import Typography from "@mui/material/Typography"
+import Divider from "@mui/material/Divider"
+import IconButton from "@mui/material/IconButton"
+import MenuIcon from "@mui/icons-material/Menu"
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft"
+import LogoutIcon from "@mui/icons-material/Logout"
+import Button from "@mui/material/Button"
+import { TableContainer } from "@mui/material"
+import { mainListItems, secondaryListItems } from "../../components/dashboard/profListItems"
+import Projects from "../../components/project/ProfProjects"
+import { AppBar, Drawer } from "../../components/dashboard/Objects"
+import { useAuthContext } from "../../hooks/useAuthContext"
+import { useLogout } from "../../hooks/useLogout"
 
-import { useProjectsContext } from '../../hooks/useProjectsContext'
+import { useProjectsContext } from "../../hooks/useProjectsContext"
 
-const mdTheme = createTheme();
+const mdTheme = createTheme()
 
 function DashboardContent() {
-  const [open, setOpen] = React.useState(true);
-  const toggleDrawer = () => {
-    setOpen(!open);
-  };
-
-  const { logout } = useLogout();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    logout();
-  };
-
-  const { dispatch } = useProjectsContext()
-  const { user } = useAuthContext();
-
-  useEffect(() => {
-    const fetchProf = async () => {
-      const response = await fetch('/profs', {
-        headers: { 'Authorization': `Bearer ${user.token}` }
-      })
-      const json = await response.json()
-
-      if (response.ok) {
-        dispatch({ type: 'SET_PROF', payload: json })
-      }
+    const [open, setOpen] = React.useState(true)
+    const toggleDrawer = () => {
+        setOpen(!open)
     }
 
-    if (user) {
-      fetchProf()
+    const { logout } = useLogout()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        logout()
     }
 
-  }, [dispatch, user])
+    const { dispatch } = useProjectsContext()
+    const { user } = useAuthContext()
 
-  return (
-    <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-          open={open}
-        >
-          <Toolbar
-            sx={{
-              pr: "24px", // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: "36px",
-                ...(open && { display: "none" }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              ProSys - Professor
-            </Typography>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              {user.name}
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-            >
-              <Button
-                color="inherit"
-                size="large"
-                startIcon={<LogoutIcon />}
-                type="submit"
-              >
-                LogOut
-              </Button>
+    useEffect(() => {
+        const fetchProf = async () => {
+            const response = await fetch("/profs", {
+                headers: { Authorization: `Bearer ${user.token}` },
+            })
+            const json = await response.json()
+
+            if (response.ok) {
+                dispatch({ type: "SET_PROF", payload: json })
+            }
+        }
+
+        if (user) {
+            fetchProf()
+        }
+    }, [dispatch, user])
+
+    return (
+        <ThemeProvider theme={mdTheme}>
+            <Box sx={{ display: "flex" }}>
+                <CssBaseline />
+                <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open}>
+                    <Toolbar
+                        sx={{
+                            pr: "24px", // keep right padding when drawer closed
+                        }}
+                    >
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={toggleDrawer}
+                            sx={{
+                                marginRight: "36px",
+                                ...(open && { display: "none" }),
+                            }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+                            ProSys - Professor
+                        </Typography>
+                        <Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+                            {user.name}
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit}>
+                            <Button color="inherit" size="large" startIcon={<LogoutIcon />} type="submit">
+                                LogOut
+                            </Button>
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+
+                <Drawer variant="permanent" open={open}>
+                    <Toolbar
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "flex-end",
+                            px: [1],
+                        }}
+                    >
+                        <IconButton onClick={toggleDrawer}>
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </Toolbar>
+
+                    <Divider />
+
+                    <List>
+                        {mainListItems}
+                        <Divider sx={{ my: 1 }} />
+                        {secondaryListItems}
+                    </List>
+                </Drawer>
+                <Box
+                    component="main"
+                    sx={{
+                        backgroundColor: (theme) =>
+                            theme.palette.mode === "light" ? theme.palette.grey[100] : theme.palette.grey[900],
+                        flexGrow: 1,
+                        height: "100vh",
+                        overflow: "auto",
+                    }}
+                >
+                    <Toolbar />
+                    <TableContainer>
+                        <Projects />
+                    </TableContainer>
+                </Box>
             </Box>
-          </Toolbar>
-        </AppBar>
-
-        <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-
-          <Divider />
-
-          <List>
-            {mainListItems}
-            <Divider sx={{ my: 1 }} />
-            {secondaryListItems}
-          </List>
-        </Drawer>
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === "light"
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: "100vh",
-            overflow: "auto",
-          }}
-        >
-          <Toolbar />
-          <TableContainer>
-            <Projects />
-          </TableContainer>
-        </Box>
-      </Box>
-    </ThemeProvider>
-  );
+        </ThemeProvider>
+    )
 }
 
 export default function Dashboard() {
-  return <DashboardContent />;
+    return <DashboardContent />
 }
