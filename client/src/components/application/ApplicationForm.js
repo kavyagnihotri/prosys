69bc3e75c7f678d045ba2feab339c3f731378d91
Kavyanjali Agnihotri <kavyagnihotri17@gmail.com>
2023-03-21 22:19:44 +0530

    Add Navbar

--- client/src/components/application/ApplicationForm.js
+++ client/src/components/application/ApplicationForm.js
@@ -1,148 +1,209 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import * as React from "react"
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
+import Paper from "@mui/material/Paper"
+import Button from "@mui/material/Button"
+import Link from "@mui/material/Link"
+import Typography from "@mui/material/Typography"
+import Grid from "@mui/material/Grid"
+import TextField from "@mui/material/TextField"
+import ToggleButton from "@mui/material/ToggleButton"
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup"
+import Toolbar from "@mui/material/Toolbar"
+import IconButton from "@mui/material/IconButton"
+import MenuIcon from "@mui/icons-material/Menu"
+import LogoutIcon from "@mui/icons-material/Logout"
 
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useApplicationsContext } from '../../hooks/useApplicationsContext';
-import { useAuthContext } from "../../hooks/useAuthContext";
+import { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { useApplicationsContext } from "../../hooks/useApplicationsContext"
+import { useAuthContext } from "../../hooks/useAuthContext"
+import { AppBar } from "../../components/dashboard/Objects"
+import { useLogout } from "../../hooks/useLogout"
 
-const theme = createTheme();
+const theme = createTheme()
 
 const ApplicationForm = () => {
-  const navigate = useNavigate();
-  const { dispatch } = useApplicationsContext();
-  const { user } = useAuthContext();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [alignment, setType] = React.useState("1");
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(null)
+    const navigate = useNavigate()
+    const { dispatch } = useApplicationsContext()
+    const { user } = useAuthContext()
+    const { logout } = useLogout()
+    const [activeStep, setActiveStep] = React.useState(0)
+    const [alignment, setType] = React.useState("1")
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+    const [open, setOpen] = React.useState(true)
 
-  const handleToggle = (event, newAlignment) => {
-    setType(newAlignment);
-  };
-
-  const handleClick = (event) => {
-    event.preventDefault();
-    navigate('/student/dashboard');
-  }
-
-  // fetching cuz once you go back to the dashboard we need the projects and the applications
-  useEffect(() => {
-    const fetchProjects = async () => {
-      const response = await fetch('/student/projects', {
-        headers: { 'Authorization': `Bearer ${user.token}` }
-      })
-      const json = await response.json()
-
-      if (response.ok) {
-        dispatch({ type: 'SET_PROJECTS', payload: json })
-      }
+    const handleToggle = (event, newAlignment) => {
+        setType(newAlignment)
     }
 
-    const fetchApplications = async () => {
-      const response = await fetch('/student/applications', {
-        headers: { 'Authorization': `Bearer ${user.token}` }
-      })
-      const json = await response.json()
-
-      if (response.ok) {
-        dispatch({ type: 'SET_APPLICATIONS', payload: json })
-      }
+    const handleLogout = async (e) => {
+        e.preventDefault()
+        logout()
     }
 
-    if (user) {
-      fetchProjects()
-      fetchApplications()
+    const handleClick = (event) => {
+        event.preventDefault()
+        navigate("/student/dashboard")
     }
 
-  }, [dispatch, user])
+    // fetching cuz once you go back to the dashboard we need the projects and the applications
+    useEffect(() => {
+        const fetchProjects = async () => {
+            const response = await fetch("/student/projects", {
+                headers: { Authorization: `Bearer ${user.token}` },
+            })
+            const json = await response.json()
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+            if (response.ok) {
+                dispatch({ type: "SET_PROJECTS", payload: json })
+            }
+        }
 
-    const studentregex = new RegExp("[fhp][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].*");
+        const fetchApplications = async () => {
+            const response = await fetch("/student/applications", {
+                headers: { Authorization: `Bearer ${user.token}` },
+            })
+            const json = await response.json()
 
-    if (!user || !studentregex.test(user.email)) {
-      setError("Student must be logged in");
-      return;
+            if (response.ok) {
+                dispatch({ type: "SET_APPLICATIONS", payload: json })
+            }
+        }
+
+        if (user) {
+            fetchProjects()
+            fetchApplications()
+        }
+    }, [dispatch, user])
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+
+        const studentregex = new RegExp("[fhp][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].*")
+
+        if (!user || !studentregex.test(user.email)) {
+            setError("Student must be logged in")
+            return
+        }
+
+        const data = new FormData(event.currentTarget)
+        const projectID = data.get("projectID")
+        const studentEmail = user.email
+        const sop = data.get("sop")
+        const type = parseInt(alignment)
+
+        //
+
+        const application = { projectID, studentEmail, type, sop }
+
+        const response = await fetch("/student/createApplication", {
+            method: "POST",
+            body: JSON.stringify(application),
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${user.token}`,
+            },
+        })
+
+        const json = await response.json()
+
+        if (!response.ok) {
+            setError(json.error)
+            setIsLoading(false)
+        }
+
+        if (response.ok) {
+            setError(null)
+
+            dispatch({ type: "CREATE_APPLICATION", payload: json })
+            setIsLoading(false)
+            setActiveStep(1)
+        }
     }
 
-    const data = new FormData(event.currentTarget);
-    const projectID = data.get('projectID')
-    const studentEmail = user.email
-    const sop = data.get('sop')
-    const type = parseInt(alignment)
-
-    // 
-
-    const application = { projectID, studentEmail, type, sop }
-
-    const response = await fetch("/student/createApplication", {
-      method: "POST",
-      body: JSON.stringify(application),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${user.token}`,
-      },
-    });
-
-    const json = await response.json()
-
-    if (!response.ok) {
-      setError(json.error)
-      setIsLoading(false)
-    }
-
-    if (response.ok) {
-      setError(null)
-      
-      dispatch({ type: "CREATE_APPLICATION", payload: json });
-      setIsLoading(false)
-      setActiveStep(1);
-    }
-  };
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-          <Typography component="h1" variant="h4" align="center">
-            Application
-          </Typography>
-          {activeStep === 1 ? (
-            <React.Fragment>
-              <p></p>
-              <Typography variant="h5" gutterBottom>
-                Your Application is Submitted.
-              </Typography>
-              <Button
-                component={Link}
-                to='/student/dashboard'
-                variant="contained"
-                sx={{ mt: 3, ml: 1 }}
-                onClick={handleClick}
-              >
-                Go to Home
-              </Button>
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                <Typography variant="h6" gutterBottom>
-                  Details
-                </Typography>
-                <Grid container spacing={3}>
-                  {/* <Grid item xs={12}>
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+                <Box
+                    sx={{
+                        marginTop: 10,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <AppBar position="absolute">
+                        <Toolbar sx={{ pr: "24px" }}>
+                            <IconButton
+                                edge="start"
+                                color="inherit"
+                                aria-label="open drawer"
+                                sx={{
+                                    marginRight: "36px",
+                                    ...(open && { display: "none" }),
+                                }}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Button
+                                onClick={handleClick}
+                                component="h1"
+                                variant="h6"
+                                noWrap
+                                color="inherit"
+                                size="large"
+                            >
+                                ProSys - Student
+                            </Button>
+                            <Typography
+                                component="h1"
+                                variant="h6"
+                                color="inherit"
+                                align="center"
+                                noWrap
+                                sx={{ flexGrow: 1 }}
+                            >
+                                {user.email}
+                            </Typography>
+                            <Box component="form" noValidate onSubmit={handleLogout}>
+                                <Button color="inherit" size="large" startIcon={<LogoutIcon />} type="submit">
+                                    LogOut
+                                </Button>
+                            </Box>
+                        </Toolbar>
+                    </AppBar>
+                </Box>
+                <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+                    <Typography component="h1" variant="h4" align="center">
+                        Application
+                    </Typography>
+                    {activeStep === 1 ? (
+                        <React.Fragment>
+                            <p></p>
+                            <Typography variant="h5" gutterBottom>
+                                Your Application is Submitted.
+                            </Typography>
+                            <Button
+                                component={Link}
+                                to="/student/dashboard"
+                                variant="contained"
+                                sx={{ mt: 3, ml: 1 }}
+                                onClick={handleClick}
+                            >
+                                Go to Home
+                            </Button>
+                        </React.Fragment>
+                    ) : (
+                        <React.Fragment>
+                            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                                <Typography variant="h6" gutterBottom>
+                                    Details
+                                </Typography>
+                                <Grid container spacing={3}>
+                                    {/* <Grid item xs={12}>
                     <TextField
                       required
                       id="title"
@@ -154,64 +215,69 @@ const ApplicationForm = () => {
                       disabled="true"
                     />
                   </Grid> */}
-                  <Grid item xs={12} sm={6}>
-                    <ToggleButtonGroup value={alignment} exclusive onChange={handleToggle} aria-label="project-type" >
-                      <ToggleButton value='1'>Formal</ToggleButton>
-                      <ToggleButton value='0'>Informal</ToggleButton>
-                    </ToggleButtonGroup>
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      required
-                      id="projectID"
-                      name="projectID"
-                      // value="projectID"
-                      label="Project ID"
-                      fullWidth
-                      variant="standard"
-                    // disabled="true"
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      id="sop"
-                      name="sop"
-                      label="Statement of Purpose"
-                      fullWidth
-                      variant="standard"
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      // required
-                      id="studentEmail"
-                      name="studentEmail"
-                      label="Your Email"
-                      fullWidth
-                      value={(JSON.parse(localStorage.getItem('user'))).email}
-                      disabled="true"
-                      variant="standard"
-                    />
-                  </Grid>
-                </Grid>
-                <Grid item xs={12} justifyContent="center" alignItems="center">
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    sx={{ mt: 3, ml: 1 }}
-                    disabled={isLoading}
-                  >
-                    Submit
-                  </Button>
-                  {error && <div className="error">{error}</div>}
-                </Grid>
-              </Box>
-            </React.Fragment>
-          )}
-        </Paper>
-      </Container>
-    </ThemeProvider>
-  );
+                                    <Grid item xs={12} sm={6}>
+                                        <ToggleButtonGroup
+                                            value={alignment}
+                                            exclusive
+                                            onChange={handleToggle}
+                                            aria-label="project-type"
+                                        >
+                                            <ToggleButton value="1">Formal</ToggleButton>
+                                            <ToggleButton value="0">Informal</ToggleButton>
+                                        </ToggleButtonGroup>
+                                    </Grid>
+                                    <Grid item xs={12} sm={6}>
+                                        <TextField
+                                            required
+                                            id="projectID"
+                                            name="projectID"
+                                            // value="projectID"
+                                            label="Project ID"
+                                            fullWidth
+                                            variant="standard"
+                                            // disabled="true"
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            id="sop"
+                                            name="sop"
+                                            label="Statement of Purpose"
+                                            fullWidth
+                                            variant="standard"
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            // required
+                                            id="studentEmail"
+                                            name="studentEmail"
+                                            label="Your Email"
+                                            fullWidth
+                                            value={JSON.parse(localStorage.getItem("user")).email}
+                                            disabled="true"
+                                            variant="standard"
+                                        />
+                                    </Grid>
+                                </Grid>
+                                <Grid item xs={12} justifyContent="center" alignItems="center">
+                                    <Button
+                                        type="submit"
+                                        variant="contained"
+                                        sx={{ mt: 3, ml: 1 }}
+                                        disabled={isLoading}
+                                    >
+                                        Submit
+                                    </Button>
+                                    {error && <div className="error">{error}</div>}
+                                </Grid>
+                            </Box>
+                        </React.Fragment>
+                    )}
+                </Paper>
+            </Container>
+        </ThemeProvider>
+    )
 }
 
 export default ApplicationForm
