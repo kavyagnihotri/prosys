c9b390363f39a5932600948e778bb8a35d02980a
Ankesh Pandey <77636343+gibsonjackson@users.noreply.github.com>
2023-03-22 05:36:52 +0530

    Chat Portal Completed

--- server/server.js
+++ server/server.js
@@ -1,22 +1,21 @@
 require("dotenv").config()
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require('cors');  
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const cors = require("cors")
 
 // express app
-const app = express();
-const projectRoutes = require('./routes/projects')
-const studentRoutes = require('./routes/student')
-const augsdRoutes = require('./routes/augsd')
-const profRoutes = require('./routes/prof')
+const app = express()
+const projectRoutes = require("./routes/projects")
+const studentRoutes = require("./routes/student")
+const augsdRoutes = require("./routes/augsd")
+const profRoutes = require("./routes/prof")
 
 // middleware
 // app.use(bodyParser())
 app.use(express.json())
-app.use(cors());
+app.use(cors())
 app.use((req, res, next) => {
-    
     next()
 })
 // app.use(bodyParser.json())
@@ -24,20 +23,38 @@ app.use((req, res, next) => {
 // app.use(bodyParser.urlencoded({ extended: true }))
 
 // routes
-app.use('/projects', projectRoutes)
-app.use('/student', studentRoutes)
-app.use('/augsd',augsdRoutes)
-app.use('/prof', profRoutes)
+app.use("/projects", projectRoutes)
+app.use("/student", studentRoutes)
+app.use("/augsd", augsdRoutes)
+app.use("/prof", profRoutes)
 
 // db connect and listen for reqs
-mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,}) 
+mongoose.set("strictQuery", false)
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+    })
     .then(() => {
-        app.listen(process.env.PORT, () => {
-            
-        })
+        app.listen(process.env.PORT, () => {})
     })
-    .catch((error) => {
-        
-    })
+    .catch((error) => {})
+
+//Chat API is used here onwards
+app.use(cors({ origin: true }))
+const axios = require("axios")
+
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body
+    // Get or create user on Chat Engine!
+    console.log("username = " + username)
+    try {
+        const r = await axios.put(
+            "https://api.chatengine.io/users/",
+            { username: username, secret: username, first_name: username },
+            { headers: { "Private-Key": "62486550-2ecc-423a-b005-bd5d1831445d" } }
+        )
+        return res.status(r.status).json(r.data)
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data)
+    }
+})
