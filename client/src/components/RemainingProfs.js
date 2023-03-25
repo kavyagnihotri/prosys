b0356aa3b0041ba96c52434fe2373cd5864de00f
JaPyC <khooshrin123@gmail.com>
2023-03-25 10:48:09 +0530

    Prevent Repeated Dept from Being appointed

--- client/src/components/RemainingProfs.js
+++ client/src/components/RemainingProfs.js
@@ -10,6 +10,12 @@ import { useState,useEffect,useRef } from 'react';
 import { useProjectsContext } from '../hooks/useProjectsContext'
 import { useAuthContext } from '../hooks/useAuthContext'
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useNavigate } from 'react-router-dom'
 
 function preventDefault(event) {
   event.preventDefault();
@@ -19,6 +25,7 @@ export default function RemainingProfs() {
 
   const { projects, dispatch } = useProjectsContext()
   const { user } = useAuthContext()
+  const navigate = useNavigate()
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -39,12 +46,47 @@ export default function RemainingProfs() {
 
   }, [dispatch, user])
 
-  const Appoint = async (id) => {
-    // 
+  const [open, setOpen] = React.useState(true);
+  var change = -1
+  var disAl = -1
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleNo = () => {
+    change=-1
+    setOpen(false);
+  };
+
+  const handleYes =() => {
+    change=1
+    setOpen(false)
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  var prof1 = null
+  var prof2 = null
+  const Appoint = async (project) => {
+    change=0
+    projects.map((p) => {
+      console.log("Finding Match")
+      if(p.dept===project.dept && p.hod===true){
+        console.log("Match Found")
+        prof1=p
+        prof2=project
+        disAl = 1
+        change = -1
+      }
+  })
+  if(change==0){
     await fetch('/prof/appoint',{
       method: 'POST', 
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({id: id})
+      body: JSON.stringify({id: project._id})
     })
     const fetchProjects = async () => {
       const response = await fetch('/prof/', {
@@ -59,9 +101,48 @@ export default function RemainingProfs() {
     }
 
     if (user) {
+      change = -1
       fetchProjects()
     }
   }
+  else if(change==1){
+    await fetch('/prof/appoint',{
+      method: 'POST', 
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({id: project._id})
+    })
+    await fetch('/prof/dismiss',{
+      method: 'POST', 
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({id: prof._id})
+    })
+    const fetchProjects = async () => {
+      const response = await fetch('/prof/', {
+        method: "POST",
+        headers: { 'Authorization': `Bearer ${user.token}` }
+      })
+      const json = await response.json()
+
+      if (response.ok) {
+        dispatch({ type: 'SET_PROJECTS', payload: json })
+      }
+    }
+
+    if (user) {
+      change = -1
+      fetchProjects()
+    }
+  }
+  // else if(change == -1){
+  //   console.log("trying open")
+  //   handleClickOpen()
+  // }
+  // else{
+  //   if (user) {
+  //     fetchProjects()
+  //   }
+  // }
+}
 
   return (
     <React.Fragment>
@@ -76,16 +157,40 @@ export default function RemainingProfs() {
         </TableHead>
         <TableBody>
           {projects && projects.map((project) => (
-            project.hod===false &&
+            project.hod===false && change===-1 &&
             <TableRow key={project._id}>
               <TableCell>{project.name}</TableCell>
               <TableCell>{project.dept}</TableCell>
-              <TableCell><Button size="large" startIcon={<PersonAddAlt1Icon />} type="submit" onClick={(e) => Appoint(project._id)}>APPOINT AS HOD</Button>
+              <TableCell><Button size="large" startIcon={<PersonAddAlt1Icon />} type="submit" onClick={(e) => Appoint(project)}>APPOINT AS HOD</Button>
               </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
+    {/* {disAl===1 && ( */}
+    { prof1!=null && prof2!=null && (
+      <Dialog
+    open={open}
+    onClose={handleClose}
+    aria-labelledby="alert-dialog-title"
+    aria-describedby="alert-dialog-description"
+    >
+    <DialogTitle id="alert-dialog-title">
+      {"HOD Conflict"}
+    </DialogTitle>
+    <DialogContent>
+      <DialogContentText id="alert-dialog-description">
+        {prof1.name} is already HOD for {prof1.dept} department.
+        Do you want to dismiss them and appoint {prof2.name} as HOD?
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={handleNo}>No</Button>
+      <Button onClick={handleYes} autoFocus>
+        Yes
+      </Button>
+    </DialogActions>
+  </Dialog> )}
     </React.Fragment>
   );
 }
