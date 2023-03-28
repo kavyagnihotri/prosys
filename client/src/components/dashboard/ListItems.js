f6034a7d17df32e676fda169a8da287edc15d53b
Kavyanjali Agnihotri <kavyagnihotri17@gmail.com>
2023-03-28 11:33:16 +0530

    Application page, move logout button

--- client/src/components/dashboard/ListItems.js
+++ client/src/components/dashboard/ListItems.js
@@ -6,37 +6,55 @@ import ListSubheader from "@mui/material/ListSubheader"
 import DashboardIcon from "@mui/icons-material/Dashboard"
 import PeopleIcon from "@mui/icons-material/People"
 import LayersIcon from "@mui/icons-material/Layers"
+import LogoutIcon from "@mui/icons-material/Logout"
+import Divider from "@mui/material/Divider"
 import AssignmentIcon from "@mui/icons-material/Assignment"
 import Link from "@mui/material/Link"
+import { useLogout } from "../../hooks/useLogout"
 
-export const mainListItems = (
-    <React.Fragment>
-        <ListItemButton component={Link} to="/student/dashboard">
-            <ListItemIcon>
-                <DashboardIcon />
-            </ListItemIcon>
-            <ListItemText primary="Dashboard" />
-        </ListItemButton>
-        {/* <ListItemButton><ListItemIcon><LayersIcon /></ListItemIcon><ListItemText primary="Projects" /></ListItemButton> */}
-    </React.Fragment>
-)
+export default function ListItems({ onListItemClick }) {
+    const { logout } = useLogout()
 
-export const secondaryListItems = (
-    <React.Fragment>
-        <ListSubheader component="div" inset>
-            Personal
-        </ListSubheader>
-        <ListItemButton component={Link} to="/student/myApplications">
-            <ListItemIcon>
-                <AssignmentIcon />
-            </ListItemIcon>
-            <ListItemText primary="My Applications" />
-        </ListItemButton>
-        <ListItemButton component={Link} to="/student/profile">
-            <ListItemIcon>
-                <PeopleIcon />
-            </ListItemIcon>
-            <ListItemText primary="My Profile" />
-        </ListItemButton>
-    </React.Fragment>
-)
+    const handleListItemClick = (content) => {
+        onListItemClick(content)
+    }
+
+    const handleLogout = async (e) => {
+        e.preventDefault()
+        logout()
+    }
+
+    return (
+        <React.Fragment>
+            <ListItemButton button onClick={() => handleListItemClick("dashboard")}>
+                <ListItemIcon>
+                    <DashboardIcon />
+                </ListItemIcon>
+                <ListItemText primary="Dashboard" />
+            </ListItemButton>
+            {/* <ListItemButton><ListItemIcon><LayersIcon /></ListItemIcon><ListItemText primary="Projects" /></ListItemButton> */}
+            <Divider sx={{ my: 1 }} />
+            <ListSubheader component="div" inset>
+                Personal
+            </ListSubheader>
+            <ListItemButton button onClick={() => handleListItemClick("applications")}>
+                <ListItemIcon>
+                    <AssignmentIcon />
+                </ListItemIcon>
+                <ListItemText primary="My Applications" />
+            </ListItemButton>
+            <ListItemButton component={Link} to="/student/profile">
+                <ListItemIcon>
+                    <PeopleIcon />
+                </ListItemIcon>
+                <ListItemText primary="My Profile" />
+            </ListItemButton>
+            <ListItemButton onClick={handleLogout}>
+                <ListItemIcon>
+                    <LogoutIcon />
+                </ListItemIcon>
+                <ListItemText primary="Log out" />
+            </ListItemButton>
+        </React.Fragment>
+    )
+}
