d6c189a30f5a609298e0db541b6100236496a52b
Kavyanjali Agnihotri <kavyagnihotri17@gmail.com>
2023-03-28 13:51:36 +0530

    Add server side file upload api

---
+++ server/routes/file.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const filestack = require("filestack-js")
+const multer = require("multer")
+const upload = multer({ dest: "uploads/" })
+const router = require("express").Router()
+
+const client = filestack.init(process.env.FILESTACK_API_KEY)
+
+router.post("/upload", upload.single("file"), (req, res) => {
+    const { path } = req.file
+    client
+        .upload(path)
+        .then((response) => {
+            // console.log(response.url)
+            res.json(response)
+        })
+        .catch((error) => {
+            res.status(500).json(error)
+        })
+})
+
+module.exports = router
