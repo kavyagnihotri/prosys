eb450961610fd128d6f4a4ee3af18a2f96560258
Akshat Kumar <akshatkumar.2001@gmail.com>
2023-03-31 11:37:51 +0530

    Update profController.js

--- server/controllers/profController.js
+++ server/controllers/profController.js
@@ -64,21 +64,41 @@ const signupProf = async (req, res) => {
     }
 }
 
-const updateProfile = async(req, res) => {
-    const id = req.body.id
-
-    try {
-        profToUpdate = await Prof.findById(id)
-        profToUpdate.chamber = req.body.chamber
-        profToUpdate.researchInterest = req.body.researchInterest
-        profToUpdate.websites = req.body.websites
-        profToUpdate.save()
-        const user = await Prof.findByIdAndUpdate(req.user._id,profToUpdate)
-        res.status(200).json(user)
-        res.send("Updated")
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
+const getName = async(req, res) => {
+  try {
+    const user1 = await Prof.find({ email: req.body.email });
+    return user1
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-module.exports = { signupProf, loginProf, getProfs, dissmissProf, appointHOD, updateProfile }
+const updateProfile = async(req, res) => {
+    const id = req.params.id
+
+    Prof.findByIdAndUpdate({ _id: id }, {
+        $set: {
+            email: req.body.email,
+            name: req.body.name,
+            password: req.body.password,
+            dept: req.body.dept,
+            chamber: req.body.chamber,
+            researchInterest: req.body.researchInterest,
+            websites: req.body.websites,
+            hod: req.body.hod
+        }
+    })
+    .then(result=>{
+        res.status(200).json({
+            updated_result: result
+        })
+    })
+    .catch(error=>{
+        res.status(400).json({
+            error: error
+        })
+    })
+    
+}
+
+module.exports = { signupProf, loginProf, getProfs, dissmissProf, appointHOD, updateProfile, getName }
