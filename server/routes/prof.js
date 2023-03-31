5946c4a17a5423c4502d1510827ed93687d02c23
Akshat Kumar <akshatkumar.2001@gmail.com>
2023-03-31 11:37:39 +0530

    Update prof.js

--- server/routes/prof.js
+++ server/routes/prof.js
@@ -1,7 +1,7 @@
 const express = require("express")
 
 // controller
-const { signupProf, loginProf, getProfs, dissmissProf, appointHOD, updateProfile } = require("../controllers/profController")
+const { signupProf, loginProf, getProfs, dissmissProf, appointHOD, updateProfile, getName } = require("../controllers/profController")
 
 const router = express.Router()
 
@@ -18,5 +18,7 @@ router.post("/dissmiss", dissmissProf)
 router.post("/appoint", appointHOD)
 
 router.put('/:id', updateProfile)
+
+router.post('/:id',getName)
 
 module.exports = router
