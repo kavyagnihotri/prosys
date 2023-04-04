4102e1d801a9a12fc35bcf4ea8e4849e009eb019
Kavyanjali Agnihotri <kavyagnihotri17@gmail.com>
2023-04-04 18:41:57 +0530

    Retry with streamifier

--- server/routes/file.js
+++ server/routes/file.js
@@ -1,36 +1,23 @@
-const { clientURL } = require("../../client/src/utils/constants")
-
 const express = require("express")
 const filestack = require("filestack-js")
 const multer = require("multer")
+const streamifier = require("streamifier")
 const upload = multer({
-    storage: multer.memoryStorage(),
     limits: { fileSize: 1000000 }, // Limit file size to 1MB
 })
-const cors = require("cors")
 const router = require("express").Router()
 
 const client = filestack.init(process.env.FILESTACK_API_KEY)
 
-const corsOptions = {
-    origin: clientURL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    // exposedHeaders: ["Authorization"],
-    maxAge: 86400,
-}
-
-router.post("/upload", cors(corsOptions), upload.single("file"), (req, res) => {
-    const { buffer } = req.file
-    client
-        .upload(buffer)
-        .then((response) => {
-            console.log(response.url)
-            res.json(response)
-        })
-        .catch((error) => {
-            res.status(500).json(error)
-        })
+router.post("/upload", upload.single("file"), async (req, res) => {
+    const { buffer, originalname } = req.file
+    const stream = streamifier.createReadStream(buffer)
+    try {
+        const response = await client.upload(stream, { filename: originalname })
+        res.json(response)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 module.exports = router
