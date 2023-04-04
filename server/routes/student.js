d4f0fb43a0dd3d4024108f064167b2bfe33e3eb4
Khooshrin Aspi Pithawalla <88196065+Khooshrin@users.noreply.github.com>
2023-04-04 19:49:02 +0530

    Server Waitlist Files

--- server/routes/student.js
+++ server/routes/student.js
@@ -1,31 +1,40 @@
-const express = require('express')
+const express = require("express")
 
 // controller fucntion
-const { signupStudent, loginStudent , getStudents} = require('../controllers/studentController')
-const { getProjects, getProject } = require('../controllers/projectController')
-const { getApplications, createApplication, deleteApplication } = require('../controllers/applicationController')
+const { signupStudent, loginStudent, getStudents } = require("../controllers/studentController")
+const { getProjects, getProject } = require("../controllers/projectController")
+const {
+    getApplications,
+    createApplication,
+    deleteApplication,
+    addScore,
+    getRanked,
+    updateStatus,
+} = require("../controllers/applicationController")
 
 const router = express.Router()
 
 // login route
-router.post('/login', loginStudent)
+router.post("/login", loginStudent)
 
 // singup route
-router.post('/signup', signupStudent)
+router.post("/signup", signupStudent)
 
-const requireAuth = require('../middleware/requireAuth')
+const requireAuth = require("../middleware/requireAuth")
 router.use(requireAuth)
 
 // Projects
-router.get('/projects', getProjects)
-router.get('/projects/:id', getProject)
+router.get("/projects", getProjects)
+router.get("/projects/:id", getProject)
 
 // Applications
-router.get('/applications', getApplications)
-router.post('/createApplication', createApplication)
+router.get("/applications", getApplications)
+router.post("/createApplication", createApplication)
+router.post("/score", addScore)
+router.get("/rank", getRanked)
+router.post("/status", updateStatus)
 
-
-router.get('/',getStudents)
+router.get("/", getStudents)
 // router.delete('/applications/:id', deleteApplication)
 // router.patch('/:id', updateProject)
 
