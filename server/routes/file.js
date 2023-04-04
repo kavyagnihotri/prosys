f6a301657b73b6d57014b7c0ec9fac7bf4cf1aec
Kavyanjali Agnihotri <kavyagnihotri17@gmail.com>
2023-04-04 19:38:21 +0530

    Remove streamifier

--- server/routes/file.js
+++ server/routes/file.js
@@ -2,6 +2,15 @@ const express = require("express")
 const filestack = require("filestack-js")
 const multer = require("multer")
 const streamifier = require("streamifier")
+const cors = require("cors")
+const corsOptions = {
+    origin: "https://prosys-client.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    // exposedHeaders: ["Authorization"],
+    maxAge: 86400,
+}
+
 const upload = multer({
     limits: { fileSize: 1000000 }, // Limit file size to 1MB
 })
@@ -9,11 +18,13 @@ const router = require("express").Router()
 
 const client = filestack.init(process.env.FILESTACK_API_KEY)
 
-router.post("/upload", upload.single("file"), async (req, res) => {
-    const { buffer, originalname } = req.file
-    const stream = streamifier.createReadStream(buffer)
+router.post("/upload", cors(corsOptions), upload.single("file"), async (req, res) => {
+    const { buffer } = req.file
+    // const stream = streamifier.createReadStream(buffer)
+    console.log(buffer)
     try {
-        const response = await client.upload(stream, { filename: originalname })
+        const response = await client.upload(buffer)
+        console.log(response)
         res.json(response)
     } catch (error) {
         res.status(500).json(error)
