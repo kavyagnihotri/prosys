0f04c5f7ce9909456ff81dae46913abb8d51a62f
Akshat Kumar <akshatkumar.2001@gmail.com>
2023-04-06 23:56:38 +0530

    Update ProfProjectDetails.js

--- client/src/components/project/ProfProjectDetails.js
+++ client/src/components/project/ProfProjectDetails.js
@@ -3,13 +3,28 @@ import TableRow from "@mui/material/TableRow"
 import OpenInNewIcon from "@mui/icons-material/OpenInNew"
 import Button from "@mui/material/Button"
 import { useNavigate } from "react-router-dom"
+import { useAuthContext } from "../../hooks/useAuthContext"
+import { useEffect, useState } from "react"
+import { useProjectsContext } from "../../hooks/useProjectsContext"
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const Project = ({ onViewApplication, project, tab }) => {
     const navigate = useNavigate()
+    const { user } = useAuthContext()
+    const { dispatch } = useProjectsContext()
 
     const onView = async (id, numberOfStudents) => {
         onViewApplication(id, numberOfStudents)
         // navigate("/prof/project/view/" + id)
+    }
+
+    const deleteProject = async (id) => {
+        const response = await fetch(`/projects/${id}`, {
+            method: "DELETE",
+            headers: { 'Authorization': `Bearer ${user.token}` }
+        })
+        const json = await response.json()
+
     }
 
     return (
@@ -29,6 +44,17 @@ const Project = ({ onViewApplication, project, tab }) => {
                     ></Button>
                 </TableCell>
             )}
+            <TableCell align="center">
+                <Button
+                        size="large"
+                        startIcon={<DeleteIcon />}
+                        type="submit"
+                    onClick={(e) => {
+                        deleteProject(project._id)
+                        window.location.reload(true)
+                    }}
+                ></Button>
+            </TableCell>
             {tab === -1 && <TableCell align="center">{project.recommendation}</TableCell>}
         </TableRow>
     )
