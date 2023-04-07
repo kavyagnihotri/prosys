3ea4a16c1c360797f14f170235ef50fc56642088
Kavyanjali Agnihotri <kavyagnihotri17@gmail.com>
2023-04-08 00:07:55 +0530

    Remove cors from client side

--- client/src/components/application/InformalApplications.js
+++ client/src/components/application/InformalApplications.js
@@ -124,7 +124,6 @@ export default function FormalApplications({ projectID, numberOfStudents, onList
         const response = await fetch(serverURL + "/student/score", {
             method: "POST",
             headers: { "Content-Type": "application/json", Authorization: `Bearer ${user.token}` },
-            mode: "no-cors", // set the mode property to 'no-cors'
             body: JSON.stringify({ appId: appId, newScore: newScore }),
         })
         const json = await response.json()
@@ -137,7 +136,6 @@ export default function FormalApplications({ projectID, numberOfStudents, onList
         const response = await fetch(serverURL + "/student/status", {
             method: "POST",
             headers: { "Content-Type": "application/json", Authorization: `Bearer ${user.token}` },
-            mode: "no-cors", // set the mode property to 'no-cors'
             body: JSON.stringify({ appId: appId, status: appStatus }),
         })
         const json = await response.json()
