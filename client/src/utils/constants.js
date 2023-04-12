9223f225e5cd77bb70aa0f3356c6215c3fc7deab
Khooshrin Aspi Pithawalla <88196065+Khooshrin@users.noreply.github.com>
2023-04-13 01:03:14 +0530

    Revert URL

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,4 @@
-//const serverURL = "https://prosys.vercel.app"
-const serverURL = "https://prosys-backend.cyclic.app"
+const serverURL = "https://prosys.vercel.app"
 const clientURL = "https://prosys-client.vercel.app"
 
 module.exports = { serverURL, clientURL }
