8c6843ac93bbb77c61bc8facbc72dfb9f2f24dbc
Khooshrin Aspi Pithawalla <88196065+Khooshrin@users.noreply.github.com>
2023-04-13 00:32:10 +0530

    Checking if it works with Cyclic

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,5 @@
-const serverURL = "https://prosys.vercel.app"
+//const serverURL = "https://prosys.vercel.app"
+const serverURL = "https://prosys-backend.cyclic.app"
 const clientURL = "https://prosys-client.vercel.app"
 
 module.exports = { serverURL, clientURL }
