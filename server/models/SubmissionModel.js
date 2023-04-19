4b4451288efbfba6e2d9cf2bba27c771e641dc16
Ankesh Pandey <77636343+gibsonjackson@users.noreply.github.com>
2023-04-19 17:16:30 +0530

    Created Models

---
+++ server/models/SubmissionModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const submissionSchema = new Schema({
+    studentemail: {
+        type: String,
+        required: true,
+    },
+    projectID: {
+        type: String,
+        required: true,
+    },
+    submissionLink: {
+        type: String,
+    },
+})
+
+module.exports = mongoose.model("submissions", submissionSchema)
