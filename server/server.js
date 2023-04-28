3eb2c76ebb94511dc1be9a07892fd3ecc8199f2e
Ankesh Pandey <77636343+gibsonjackson@users.noreply.github.com>
2023-04-28 13:46:05 +0530

    NEW CHAT APIS

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.post("/authenticate", async (req, res) => {
         const r = await axios.put(
             "https://api.chatengine.io/users/",
             { username: username, secret: username, first_name: username },
-            { headers: { "Private-Key": "66fb706d-7c8c-4c26-8edc-49dd33ea1038" } }
+            { headers: { "Private-Key": "1654474a-a136-4def-951d-c8c0670bd2b8" } }
         )
         return res.status(r.status).json(r.data)
     } catch (e) {
